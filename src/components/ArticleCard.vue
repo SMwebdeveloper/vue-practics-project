2dda33aad28fb5d4709e5b5a10cc96e369f4b62b
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2022-11-27 17:14:00 +0500

    article card

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <h5 class="card-title">
+                    {{article.title}}
+                </h5>
+                <p class="card-text">
+                   {{article.body}}
+                </p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                </div>
+              </div>  
+            </div>
+        </div>
+</template>
+
+<script>
+ export default {
+    props: {
+        article:{
+            type: Object,
+            required: true
+        }
+    }
+ }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h2>Home</h2>
-    {{data}}
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <ArticleCardVue v-for="article in data" :article="article" :key="article.id"/>
+    </div>
   </div>
 </template>
 <script>
+import ArticleCardVue from "../components/ArticleCard.vue";
 import { mapState } from "vuex";
 export default {
+    components: {
+      ArticleCardVue
+    },
   computed: {
     ...mapState({
       data: (state) => state.articles.data,
