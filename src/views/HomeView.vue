1588578e7f067ad0509ca9ecbd1d0a9bacff4380
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2022-11-27 16:19:56 +0500

    articles api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
-        <h2>Home</h2>
-    </div>
+  <div>
+    <h2>Home</h2>
+    {{data}}
+  </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
-    
-}
+  computed: {
+    ...mapState({
+      data: (state) => state.articles.data,
+      isLoading: (state) => state.articles.isLoading,
+      error: (state) => state.articles.error,
+    }),
+  },
+  mounted() {
+    this.$store.dispatch('articles')
+  }
+};
 </script>
-<style>
-    
-</style>
+<style></style>
