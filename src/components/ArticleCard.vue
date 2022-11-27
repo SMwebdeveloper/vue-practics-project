b7281b24ecd7c99ab0aa6ea2eb69a7458a76ceac
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2022-11-27 19:18:04 +0500

    loader

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -8,11 +8,12 @@
                 <p class="card-text">
                    {{article.body}}
                 </p>
-                <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex justify-content-between align-items-center card-footer">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
+                    <small class="text-muted">{{new Date(article.createdAt).toLocaleString("us")}}</small>
                 </div>
               </div>  
             </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <ArticleCardVue v-for="article in data" :article="article" :key="article.id"/>
+        <Loader v-if="isLoading" class="offset-md-6"/>
+        <ArticleCardVue  v-for="article in data" :article="article" :key="article.id"/>
     </div>
   </div>
 </template>
 <script>
 import ArticleCardVue from "../components/ArticleCard.vue";
 import { mapState } from "vuex";
+import Loader from "../ui-components/Loader.vue";
 export default {
     components: {
-      ArticleCardVue
-    },
+    ArticleCardVue,
+    Loader
+},
   computed: {
     ...mapState({
       data: (state) => state.articles.data,
